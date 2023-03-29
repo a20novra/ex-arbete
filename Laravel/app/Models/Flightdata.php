682d2d866a2abfb835a01b7a81dfcac01c75a383
novin ramzi <novinramzi1@gmail.com>
2023-03-29 15:08:30 +0200

    Make all Flightdata columns fillable

--- Laravel/app/Models/Flightdata.php
+++ Laravel/app/Models/Flightdata.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Flightdata extends Model
 {
     use HasFactory;
+    protected $fillable = ['airline_iata_code', 'airline_id', 'source_airport_iata_code', 'source_airport_id','destination_airport_iata_code', 'destination_airport_id', 'number_of_stops', 'equipment'];
+
 }
